780d862765585a0958ebd64e657ae3139ede0806
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-08-12 20:44:45 +0300

    Add files via upload

---
+++ PatikaOOPInheritance/PatikaOOPInheritance/BaseKisi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaOOPInheritance
+{
+    public abstract class BaseKisi
+    {
+        public string Ad;
+        public string Soyad;
+
+        public void YazdırAdSoyad()
+        {
+            Console.WriteLine($"Adı ve soyadı : {Ad} {Soyad}");
+        }
+    
+    }
+
+    public sealed class Ogrenci : BaseKisi
+    {
+        public int OgrenciNumarasi {  get; set; }
+
+        public void YazdırOgrenci()
+        {
+            Console.WriteLine($"Öğrenci numarası : {OgrenciNumarasi}");
+            YazdırAdSoyad();
+        }
+
+    }
+    
+    public sealed class Ogretmen: BaseKisi
+    {
+        public decimal OgretmenMaasi { get; set; }  
+
+        public void YazdırOgretmen()
+        {
+            YazdırAdSoyad();
+            Console.WriteLine($"Öğretmenin maaşı: {OgretmenMaasi}");
+        }
+
+
+    }
+
+}
---
+++ PatikaOOPInheritance/PatikaOOPInheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using PatikaOOPInheritance;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        Ogrenci ogrenci = new Ogrenci();
+        ogrenci.Ad = "Betül";
+        ogrenci.Soyad = "Çelik";
+        ogrenci.OgrenciNumarasi = 11223344;
+
+        ogrenci.YazdırOgrenci();
+        Console.WriteLine();
+
+        Ogretmen ogretmen = new Ogretmen();
+        ogretmen.Ad = "Zehra";
+        ogretmen.Soyad = "Demir";
+        ogretmen.OgretmenMaasi = 52000;
+
+        ogretmen.YazdırOgretmen();
+
+
+
+    }
+}
